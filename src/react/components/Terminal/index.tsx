386a31ce4ee83d32f5e6a3c8124b4023dc17fa44
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-03-07 15:38:46 +0000

    Improve terminal layout [WIP]

--- src/react/components/Terminal/index.tsx
+++ src/react/components/Terminal/index.tsx
@@ -1,14 +1,17 @@
 import {
   useRef,
   useEffect,
+  useState,
 } from 'react'
 import type { useDevbook } from '@devbookhq/sdk'
+import { FitAddon } from 'xterm-addon-fit'
 
 import 'xterm/css/xterm.css'
 
 import Header from '../Editor/Header'
 import Separator from '../Separator'
 import useTerminal from './useTerminal'
+import SpinnerIcon from '../SpinnerIcon'
 
 export interface Props {
   devbook: Pick<ReturnType<typeof useDevbook>, 'terminal' | 'status'>
@@ -23,25 +26,48 @@ function Terminal({
 }: Props) {
   const terminalEl = useRef<HTMLDivElement>(null)
   const terminal = useTerminal({ devbook })
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(function attachTerminal() {
     if (!terminalEl.current) return
+    if (!terminal) return
 
-    terminal?.open(terminalEl.current)
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
+
+    terminal.open(terminalEl.current)
+    fitAddon.fit()
+
+    setIsLoading(false)
+
+    return () => {
+      setIsLoading(true)
+    }
   }, [terminal])
 
   return (
-    <div className={`rounded ${lightTheme ? '' : 'dark'}`}>
+    <div className={`rounded flex flex-col flex-1 ${lightTheme ? '' : 'dark'}`}>
       <Header
         filepath="Terminal"
       />
       <div
-        className="flex-1 flex max-h-full min-w-0 overflow-auto"
-        ref={terminalEl}
+        className="rounded-b overflow-auto min-w-0 max-h-full flex flex-1 bg-gray-300 dark:bg-black-650"
         style={{
           ...height && { height },
         }}
-      />
+      >
+        {/* {isLoading &&
+          <div
+            className="flex flex-1 justify-center items-center"
+          >
+            <SpinnerIcon />
+          </div>
+        } */}
+        <div
+          className="flex flex-1 min-w-0 max-h-full"
+          ref={terminalEl}
+        />
+      </div>
     </div>
   )
 }
