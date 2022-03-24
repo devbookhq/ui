35ff6c218fa7c1e21046bd58aea9ba516241dda3
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-03-23 17:52:37 -0700

    Update App.tsx

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex flex-1 min-w-0 justify-center items-center p-2">
       <Examples theme='dark' />
-      {/* <Examples theme='light' /> */}
+      <Examples theme='light' />
     </div>
   );
 }
