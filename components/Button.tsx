7a3b239c466ca7b7153bb44072b2873f20c5b572
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2022-05-05 20:11:53 +0200

    Fix build errors

--- components/Button.tsx
+++ components/Button.tsx
@@ -17,18 +17,16 @@ interface Props {
   onClick?: (e: any) => void
 }
 
-const Button = forwardRef<HTMLDivElement, Props>((props, ref) => {
-  const {
-    className,
-    text,
-    variant = Variant.Outline,
-    icon,
-    onClick,
-  } = props
+function Button({
+  className,
+  text,
+  variant = Variant.Outline,
+  icon,
+  onClick,
+}: Props) {
   if (variant === Variant.Full) {
     return (
       <button
-        ref={ref}
         className="
           py-1.5
           px-4
@@ -64,7 +62,6 @@ const Button = forwardRef<HTMLDivElement, Props>((props, ref) => {
 
   return (
     <button
-      ref={ref}
       className="
         p-[2px]
         rounded-lg
@@ -96,85 +93,7 @@ const Button = forwardRef<HTMLDivElement, Props>((props, ref) => {
       </div>
     </button>
   )
-});
-
-//function Button({
-//  className,
-//  text,
-//  variant = Variant.Outline,
-//  icon,
-//  onClick,
-//}: Props) {
-//  if (variant === Variant.Full) {
-//    return (
-//      <button
-//        className="
-//          py-1.5
-//          px-4
-//
-//          flex
-//          items-center
-//          justify-center
-//
-//          rounded-lg
-//
-//          bg-green-500
-//          hover:shadow-lg
-//          hover:shadow-green-500/50
-//        "
-//        onClick={onClick}
-//      >
-//        <div className="
-//          flex
-//          flex-row
-//          items-center
-//          justify-center
-//          space-x-2
-//        ">
-//          {icon}
-//          <Text
-//            size={Text.size.S1}
-//            text={text}
-//          />
-//        </div>
-//      </button>
-//    )
-//  }
-//
-//  return (
-//    <button
-//      className="
-//        p-[2px]
-//        rounded-lg
-//        bg-black-700
-//        hover:bg-green-gradient
-//        hover:shadow-lg
-//        hover:shadow-green-500/50
-//      "
-//      onClick={onClick}
-//    >
-//      <div className="
-//        py-[4px]
-//        px-[10px]
-//
-//        flex
-//        flex-row
-//        items-center
-//        justify-center
-//        space-x-2
-//
-//        rounded-lg
-//        bg-black-900
-//      ">
-//        {icon}
-//        <Text
-//          size={Text.size.S1}
-//          text={text}
-//        />
-//      </div>
-//    </button>
-//  )
-//}
+}
 
 Button.variant = Variant
 export default Button
