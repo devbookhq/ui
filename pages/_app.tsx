f838d2e9809debd08b9147b880cc0a35d7b79274
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-07-11 17:32:38 +0000

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'react-toastify/dist/ReactToastify.css'
 import 'styles/global.css'
 
+import Script from 'next/script'
 import { AppProps } from 'next/app'
 import { UserProvider } from '@supabase/supabase-auth-helpers/react'
 import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
@@ -11,22 +12,37 @@ import { UserInfoContextProvider } from 'utils/useUserInfo'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider supabaseClient={supabaseClient}>
-      <UserInfoContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-          <ToastContainer
-            position="bottom-right"
-            theme="dark"
-            autoClose={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable={false}
-          />
-        </Layout>
-      </UserInfoContextProvider>
-    </UserProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-BNSES8FS5J"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-BNSES8FS5J');
+      `}
+      </Script>
+      <UserProvider supabaseClient={supabaseClient}>
+        <UserInfoContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+            <ToastContainer
+              position="bottom-right"
+              theme="dark"
+              autoClose={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable={false}
+            />
+          </Layout>
+        </UserInfoContextProvider>
+      </UserProvider>
+    </>
   )
 }
