1011be38b3b3cec63c9ca19538b556d3c3b9cfc1
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-08-31 16:28:07 +0000

    Add testing example

---
+++ example/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { CodeSnippet } from '@devbookhq/react'
+
+function App() {
+  return (
+    <div className="App">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style={{ height: '200px', width: '300px' }}>
+        <CodeSnippet id='yRA8mTcSaI5S' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
