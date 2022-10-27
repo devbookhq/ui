3ea56f919cafa58e6732ff9070e5dccc42c6909a
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-10-27 12:57:47 +0000

    Use tailwind for drag layer CSS

--- src/components/AppEditor/Board/CustomDragLayer.tsx
+++ src/components/AppEditor/Board/CustomDragLayer.tsx
@@ -1,26 +1,17 @@
 import { observer } from 'mobx-react-lite'
-import type { CSSProperties } from 'react'
 
 import { useBoardDrag } from '../../../core/EditorProvider/useBoardDrag'
 
-const layerStyles: CSSProperties = {
-  position: 'fixed',
-  pointerEvents: 'none',
-  zIndex: 100,
-  left: 0,
-  top: 0,
-  width: '100%',
-  height: '100%',
-}
-
-export interface Props {}
-
-function CustomDragLayer({}: Props) {
+function CustomDragLayer() {
   const draggedChildren = useBoardDrag()
 
   if (!draggedChildren) return null
 
-  return <div style={layerStyles}>{draggedChildren}</div>
+  return (
+    <div className="pointer-events-none fixed left-0 top-0 z-50 h-full w-full">
+      {draggedChildren}
+    </div>
+  )
 }
 
 export default observer(CustomDragLayer)
