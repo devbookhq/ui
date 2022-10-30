1761b92ae7d3b9eb4a2166cf9b155ca80b445357
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-10-30 16:09:27 +0000

    Allow changing current env; Add link for editing env

--- src/components/AppEditor/InspectorSidebar/AppInspector/EnvTab.tsx
+++ src/components/AppEditor/InspectorSidebar/AppInspector/EnvTab.tsx
@@ -1,5 +1,9 @@
 import { observer } from 'mobx-react-lite'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
+import Button from 'components/Button'
+import Input from 'components/Input'
 import Text from 'components/typography/Text'
 
 import { useRootStore } from 'core/EditorProvider/models/RootStoreProvider'
@@ -7,18 +11,44 @@ import { useRootStore } from 'core/EditorProvider/models/RootStoreProvider'
 function EnvTab() {
   const { resources } = useRootStore()
 
+  const [envID, setEnvID] = useState(resources.environmentID)
+
+  useEffect(
+    function setEnv() {
+      setEnvID(resources.environmentID)
+    },
+    [resources.environmentID],
+  )
+
   return (
-    <div className="flex flex-col items-start justify-start space-y-1 px-1">
-      <Text
-        className="flex w-12 whitespace-nowrap text-slate-400"
-        size={Text.size.S3}
-        text="Environment ID"
-      />
-      <Text
-        className="flex w-12 whitespace-nowrap text-slate-600"
-        size={Text.size.S3}
-        text={resources.environmentID || 'undefined'}
-      />
+    <div className="flex min-h-0 flex-1 flex-col items-start justify-between space-y-4 px-1">
+      <div className="flex flex-1 flex-col space-y-1">
+        <Text
+          className="flex w-12 whitespace-nowrap text-slate-400"
+          size={Text.size.S3}
+          text="Environment ID"
+        />
+        <Input
+          value={envID || ''}
+          onChange={e => setEnvID(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center justify-start space-x-1">
+        <Link
+          rel="noopener noreferrer"
+          target="_blank"
+          href={`https://dash.usedevbook.com/${resources.environmentID}/edit`}
+        >
+          <Button text="Edit env" />
+        </Link>
+        {envID !== resources.environmentID && (
+          <Button
+            text="Confirm env change"
+            variant={Button.variant.Full}
+            onClick={() => resources.setEnvironmentID(envID)}
+          />
+        )}
+      </div>
     </div>
   )
 }
