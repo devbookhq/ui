f21539f4951dbfb517e470ac0681ee03ea76d2ed
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-10-30 12:00:07 +0000

    Center app blocks in the preview and deploy

--- src/components/AppView.tsx
+++ src/components/AppView.tsx
@@ -1,17 +1,61 @@
 import { UI } from 'components/AppEditor/uiComponents'
 
 import { RootState } from 'core/EditorProvider/models/RootStoreProvider'
+import { BoardBlock } from 'core/EditorProvider/models/board'
 import ViewProvider from 'core/ViewProvider'
 
 export interface Props {
   state: RootState
 }
 
+interface ViewCanvas {
+  top: number
+  left: number
+  width: number
+  height: number
+}
+
+function getViewCanvas(blocks: BoardBlock[]) {
+  return blocks.reduce<ViewCanvas | undefined>((res, block) => {
+    if (!res)
+      return {
+        width: block.left + block.width,
+        height: block.top + block.height,
+        top: block.top,
+        left: block.left,
+      }
+
+    if (res.top > block.top) res.top = block.top
+    if (res.left > block.left) res.left = block.left
+
+    if (res.height < block.top + block.height) res.height = block.top + block.height
+    if (res.width < block.left + block.width) res.width = block.left + block.width
+
+    return res
+  }, undefined)
+}
+
 function AppView({ state }: Props) {
+  const blocks = Object.values(state.board.blocks)
+
+  const viewCanvas = getViewCanvas(blocks)
+
+  console.log(blocks, viewCanvas)
+
+  if (!viewCanvas) return null
+
   return (
     <ViewProvider initialState={state}>
-      <div className="relative flex flex-1 bg-white">
-        {Object.values(state.board.blocks).map(b => (
+      <div
+        style={{
+          width: viewCanvas.width,
+          height: viewCanvas.height,
+          top: -viewCanvas.top / 2,
+          left: -viewCanvas.left / 2,
+        }}
+        className="relative m-auto flex"
+      >
+        {blocks.map(b => (
           <UI.ViewBoardBlock
             key={b.id}
             {...b}
