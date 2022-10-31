850ae64c8e429ccf900a0730dd975db04c387388
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-10-31 12:37:29 +0000

    Add header [WIP]

--- src/components/AppEditor/Board/index.tsx
+++ src/components/AppEditor/Board/index.tsx
@@ -1,12 +1,16 @@
 import clsx from 'clsx'
 import { observer } from 'mobx-react-lite'
 
+import Text from 'components/typography/Text'
+
 import { getGridStyle, xStep, yStep } from 'core/EditorProvider/grid'
 import { useRootStore } from 'core/EditorProvider/models/RootStoreProvider'
 import { canvasClass, useBoard } from 'core/EditorProvider/useBoard'
 
 import { UI } from '../uiComponents'
 import SidebarDraggingLayer from './SidebarDraggingLayer'
+
+export const headerBoardHeight = 90
 
 const gridStyle = getGridStyle(xStep, yStep, '#94a3b8')
 
@@ -22,6 +26,16 @@ function Board() {
         style={gridStyle}
         onClick={board.resetBlockSelection}
       >
+        <div
+          style={{ height: headerBoardHeight }}
+          className=" group absolute flex h-[90px] w-full flex-1 items-center justify-center border-b border-slate-200 hover:bg-slate-100/40"
+        >
+          <Text
+            className="pointer-events-none select-none text-transparent group-hover:text-slate-300"
+            text="Header"
+            size={Text.size.S1}
+          />
+        </div>
         {boardBlocks.map(b => (
           <UI.EditorBoardBlock
             {...b}
--- src/components/AppView.tsx
+++ src/components/AppView.tsx
@@ -4,6 +4,8 @@ import { RootState } from 'core/EditorProvider/models/RootStoreProvider'
 import { BoardBlock } from 'core/EditorProvider/models/board'
 import ViewProvider from 'core/ViewProvider'
 
+import { headerBoardHeight } from './AppEditor/Board'
+
 export interface Props {
   state: RootState
 }
@@ -15,51 +17,115 @@ interface ViewCanvas {
   height: number
 }
 
-function getViewCanvas(blocks: BoardBlock[]) {
-  return blocks.reduce<ViewCanvas | undefined>((res, block) => {
-    if (!res)
-      return {
-        width: block.left + block.width,
-        height: block.top + block.height,
-        top: block.top,
-        left: block.left,
-      }
+function getBoundingRectangle(blocks: BoardBlock[]) {
+  return (
+    blocks.reduce<ViewCanvas | undefined>((res, block) => {
+      if (!res)
+        return {
+          width: block.left + block.width,
+          height: block.top + block.height,
+          top: block.top,
+          left: block.left,
+        }
 
-    if (res.top > block.top) res.top = block.top
-    if (res.left > block.left) res.left = block.left
+      if (res.top > block.top) res.top = block.top
+      if (res.left > block.left) res.left = block.left
 
-    if (res.height < block.top + block.height) res.height = block.top + block.height
-    if (res.width < block.left + block.width) res.width = block.left + block.width
+      if (res.height < block.top + block.height) res.height = block.top + block.height
+      if (res.width < block.left + block.width) res.width = block.left + block.width
 
-    return res
-  }, undefined)
+      return res
+    }, undefined) || { top: 0, left: 0, width: 0, height: 0 }
+  )
 }
 
 function AppView({ state }: Props) {
   const blocks = Object.values(state.board.blocks)
-  const viewCanvas = getViewCanvas(blocks)
 
-  if (!viewCanvas) return null
+  const headerBlocks = blocks.filter(b => b.top < headerBoardHeight)
+  const contentBlocks = blocks.filter(b => b.top >= headerBoardHeight)
+
+  const contentCanvas = getBoundingRectangle(contentBlocks)
+  const headerCanvas = getBoundingRectangle(headerBlocks)
+
+  const canvasWidth = Math.max(contentCanvas.width, headerCanvas.width)
+  const canvasCenter = canvasWidth / 2
+
+  const remappedHeaderBlocks = headerBlocks.map(b => {
+    const blockCenter = b.left + b.width / 2
+    return {
+      ...b,
+      side: blockCenter > canvasCenter ? 'right' : 'left',
+    }
+  })
+
+  const leftHeaderBlocks = remappedHeaderBlocks.filter(b => b.side === 'left')
+  const rightHeaderBlocks = remappedHeaderBlocks.filter(b => b.side === 'right')
+
+  const leftHeaderCanvas = getBoundingRectangle(leftHeaderBlocks)
+  const rightHeaderCanvas = getBoundingRectangle(rightHeaderBlocks)
+
+  console.log({ leftHeaderCanvas })
 
   return (
     <div className="flex flex-1 flex-col bg-[#fbfdfd]">
       <ViewProvider initialState={state}>
-        <div
-          style={{
-            width: viewCanvas.width,
-            height: viewCanvas.height,
-            top: -viewCanvas.top / 2,
-            left: -viewCanvas.left / 2,
-          }}
-          className="relative m-auto flex"
-        >
-          {blocks.map(b => (
-            <UI.ViewBoardBlock
-              key={b.id}
-              {...b}
-            />
-          ))}
-        </div>
+        {headerCanvas && (
+          <div
+            className="flex justify-between border-b bg-[#fbfcfc]"
+            style={{ width: '100%' }}
+          >
+            <div
+              style={{
+                width: leftHeaderCanvas.width,
+                height: headerCanvas.height,
+                top: -headerCanvas.top / 2,
+              }}
+              className="relative flex"
+            >
+              {leftHeaderBlocks.map(b => (
+                <UI.ViewBoardBlock
+                  key={b.id}
+                  {...b}
+                />
+              ))}
+            </div>
+            <div
+              style={{
+                width: rightHeaderCanvas.width,
+                height: headerCanvas.height,
+                right: leftHeaderCanvas.left,
+                top: -headerCanvas.top / 2,
+              }}
+              className="relative flex"
+            >
+              {rightHeaderBlocks.map(b => (
+                <UI.ViewBoardBlock
+                  key={b.id}
+                  {...b}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {contentCanvas && (
+          <div
+            style={{
+              width: contentCanvas.width,
+              height: contentCanvas.height,
+              top: -(contentCanvas.top + headerBoardHeight) / 2,
+              left: -contentCanvas.left / 2,
+            }}
+            className="relative m-auto flex"
+          >
+            {contentBlocks.map(b => (
+              <UI.ViewBoardBlock
+                key={b.id}
+                {...b}
+              />
+            ))}
+          </div>
+        )}
       </ViewProvider>
     </div>
   )
