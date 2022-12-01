0366aa6013df52e75eb85e22110a52e92d5c2aba
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-12-01 15:54:08 +0000

    Export additional types; Create release

--- packages/devbookhq-terminal/CHANGELOG.md
+++ packages/devbookhq-terminal/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @devbookhq/terminal
 
+## 0.1.1
+
+### Patch Changes
+
+- Export additional types
+
 ## 0.1.0
 
 ### Minor Changes
--- packages/devbookhq-terminal/src/index.ts
+++ packages/devbookhq-terminal/src/index.ts
@@ -1,5 +1,6 @@
 import '../styles/index.css'
 
 export { default as Terminal } from './components/Terminal'
+export type { Handler as TerminalHandler } from './components/Terminal'
 export * from './hooks/useTerminalSession'
 export * from './hooks/useTerminalProcess'
