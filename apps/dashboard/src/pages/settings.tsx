ee242676747ca7b4bb747ba9ddc302912c5cba2c
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-01-16 14:14:58 +0000

    Add Slack App button to settings page

--- apps/dashboard/src/pages/settings.tsx
+++ apps/dashboard/src/pages/settings.tsx
@@ -91,6 +91,37 @@ function Settings() {
           <Text
             className="text-slate-400"
             size={Text.size.S3}
+            text="Slack Integration"
+          />
+
+          <ButtonLink
+            href="https://slack.com/oauth/v2/authorize?scope=incoming-webhook&amp;user_scope=&amp;redirect_uri=https%3A%2F%2F3000-devbookhq-ui-bdurd1rl9pv.ws-eu82.gitpod.io%2Fslack%2Fredirect&amp;client_id=625989678439.4574374749984"
+            text="Add to Slack"
+            icon={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ height: '16px', width: '16px' }}
+                viewBox="0 0 122.8 122.8"
+              >
+                <path
+                  d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z" fill="#e01e5a"></path><path d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z" fill="#36c5f0"></path><path d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z" fill="#2eb67d"></path><path d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
+                    fill="#ecb22e"
+                  >
+                </path>
+              </svg>
+            }
+          />
+        </div>
+        <div
+          className="
+        flex
+        flex-col
+        space-y-1
+      "
+        >
+          <Text
+            className="text-slate-400"
+            size={Text.size.S3}
             text="API Key"
           />
           {isAPIKeyLoading
@@ -111,7 +142,7 @@ function Settings() {
           />
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
