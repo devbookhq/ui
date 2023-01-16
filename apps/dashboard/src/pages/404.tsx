211620009dced73a4ca9a1bbe05df3eb75360fbe
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-01-16 14:14:27 +0000

    Improve 404 page

--- apps/dashboard/src/pages/404.tsx
+++ apps/dashboard/src/pages/404.tsx
@@ -1,7 +1,12 @@
 import Text from 'components/typography/Text'
+import { XCircle } from 'lucide-react'
 
 function Custom404() {
-  return <Text text="404 - Page Not Found" />
+  return (
+    <div className="flex flex-1 justify-center items-center">
+      <Text text="Page Not Found" size={Text.size.S1} icon={<XCircle />} />
+    </div>
+  )
 }
 
 export default Custom404
