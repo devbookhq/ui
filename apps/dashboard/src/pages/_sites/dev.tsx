fe30b3b3b433d1568f6e6e81a60d7297afb1402f
Tomas Valenta <valenta.and.thomas@gmail.com>
2023-01-31 14:38:16 +0000

    Move MDX processing to server side dev page

--- apps/dashboard/src/pages/_sites/dev.tsx
+++ apps/dashboard/src/pages/_sites/dev.tsx
@@ -4,29 +4,28 @@ import getRawBody from 'raw-body'
 
 import AppPage from 'components/AppPage'
 import { Guide } from 'guides/content/Guide'
+import { getGuideData } from 'guides/content'
+import { GuideDBEntry } from 'queries/db'
 
 export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
   // context.res.setHeader(
   //   'Cache-Control',
   //   'maxage'
   // )
-  // return {
-  //   props: {
-  //     guide: test as any,
-  //   }
-  // }
 
   // if (context.req.method === 'GET') {
   // https://github.com/vercel/next.js/discussions/12152
   const body = await getRawBody(context.req)
   const stringBody = body.toString()
   console.log('BODY >>>>>>>>>>>>>>>>>>>', stringBody)
-  const jsonBody = JSON.parse(stringBody)
+  const jsonBody = JSON.parse(stringBody) as GuideDBEntry
   console.log('JSON >>>>>>>>>>>>>>>>>>>', jsonBody)
+
+  const guide = await getGuideData(jsonBody)
 
   return {
     props: {
-      guide: JSON.parse(body.toString())
+      guide,
     }
   }
 }
